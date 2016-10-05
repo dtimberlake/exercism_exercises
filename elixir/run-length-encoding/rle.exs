727346e9b57b585c313bf36c4634f97cccb30906
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-05 14:54:53 -0500

    run-length-encoding finished

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -8,11 +8,21 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t) :: String.t
   def encode(string) do
-
+    Regex.scan(~r/([a-zA-Z])\1*/, string, [capture: :first])
+    |> List.flatten
+    |> Enum.map(&String.split(&1, ""))
+    |> Enum.map(fn(x) ->
+        "#{length(x) - 1}#{List.first x}"
+      end)
+    |> Enum.join
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
-
+    Regex.scan(~r/([1-9]*)([a-zA-Z])/, string, [capture: :all_but_first])
+    |> Enum.map(fn([n_str, str]) ->
+        String.duplicate(str, String.to_integer n_str)
+      end)
+    |> Enum.join
   end
 end
--- elixir/run-length-encoding/rle_test.exs
+++ elixir/run-length-encoding/rle_test.exs
@@ -12,27 +12,27 @@ defmodule RunLengthEncoderTest do
     assert RunLengthEncoder.encode("") === ""
   end
 
-  @tag :skip
+  # @tag :skip
   test "simple string gets encoded" do
     assert RunLengthEncoder.encode("AAA") === "3A"
   end
 
-  @tag :skip
+  # @tag :skip
   test "more complicated string" do
     assert RunLengthEncoder.encode("HORSE") == "1H1O1R1S1E"
   end
 
-  @tag :skip
+  # @tag :skip
   test "an even more complex string" do
     assert RunLengthEncoder.encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB") === "12W1B12W3B24W1B"
   end
 
-  @tag :skip
+  # @tag :skip
   test "it decodes an encoded simple string" do
     assert RunLengthEncoder.decode("3A") === "AAA"
   end
 
-  @tag :skip
+  # @tag :skip
   test "it decodes a more complicated string" do
     assert RunLengthEncoder.decode("12W1B12W3B24W1B") === "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
   end
