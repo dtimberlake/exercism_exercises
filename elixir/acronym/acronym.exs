e1b6171630674b4738432b27123928036f70e409
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-05 12:10:43 -0500

    refactored acronyms

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,14 +5,8 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    string
-    |> String.split
-    |> Enum.map(fn(x) ->
-      Enum.join([
-        String.first(x),
-        Regex.replace(~r/[a-z\d\s.,\/#!$%\^&\*;:{}=\-_`~()]/, String.slice(x, 1..-1), "")
-      ])
-    end)
+    Regex.scan(~r/[A-Z]|(?<= )[a-z]/, string)
+    |> List.flatten
     |> Enum.join("")
     |> String.upcase
   end
