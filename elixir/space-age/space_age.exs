0f41fe42236cc73c2cb803d558276b1bf9557a97
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-05 09:50:13 -0500

    space_age completed

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -2,12 +2,29 @@ defmodule SpaceAge do
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :neptune | :uranus
 
+  @earth_seconds_per_orbit 31557600
+
+  @relative_orbit_times %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-
+    relative_orbit_time = Map.fetch!(@relative_orbit_times, planet)
+    seconds / seconds_per_orbit relative_orbit_time
   end
+
+  @spec seconds_per_orbit(integer) :: float
+  def seconds_per_orbit(relative_orbit_time), do: @earth_seconds_per_orbit * relative_orbit_time
 end
