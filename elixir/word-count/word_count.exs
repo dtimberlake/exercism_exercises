cee01dbecd89a039083f16a590c4f25878d1e609
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-05 11:45:35 -0500

    word_count passing

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,4 +1,8 @@
 defmodule Words do
+  import String
+  import List, only: [foldr: 3]
+  import Map, only: [update: 4]
+
   @doc """
   Count the number of words in the sentence.
 
@@ -6,6 +10,15 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
+    words =
+      sentence
+      |> String.replace(~r/_/, " ")
+      |> String.replace(~r/[.,\/#!$%\^&\*;:{}=`~()@]/, "")
+      |> String.downcase
+      |> String.split
 
+    foldr words, %{}, fn(x, acc) ->
+      update(acc, x, 1, &(&1 + 1))
+    end
   end
 end
