5aff079528bd5622adac85da238ab47b10c56032
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-05 12:06:25 -0500

    acronym completed

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,5 +5,15 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.split
+    |> Enum.map(fn(x) ->
+      Enum.join([
+        String.first(x),
+        Regex.replace(~r/[a-z\d\s.,\/#!$%\^&\*;:{}=\-_`~()]/, String.slice(x, 1..-1), "")
+      ])
+    end)
+    |> Enum.join("")
+    |> String.upcase
   end
 end
