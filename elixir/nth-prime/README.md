363b3db20ce461301c3d6317d76b49bc2fcce6ba
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-07 18:17:54 -0500

    nth-prime completed

--- elixir/nth-prime/README.md
+++ elixir/nth-prime/README.md
@@ -1,5 +1,7 @@
 # Nth Prime
 
+Submission Link: [http://exercism.io/submissions/c83a13f761d24592aae307d3c5bf4273](http://exercism.io/submissions/c83a13f761d24592aae307d3c5bf4273)
+
 Write a program that can tell you what the nth prime is.
 
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
@@ -51,4 +53,3 @@ A variation on Problem 7 at Project Euler [http://projecteuler.net/problem=7](ht
 
 ## Submitting Incomplete Problems
 It's possible to submit an incomplete solution so you can see how others have completed the exercise.
-
--- elixir/nth-prime/nth_prime.exs
+++ elixir/nth-prime/nth_prime.exs
@@ -1,10 +1,36 @@
 defmodule Prime do
+  defmodule Model do
+    alias :math, as: Math
+    defstruct target_n: 2, last_n: 1, last_prime: 2, num: 3, div: 3, divMax: 1
+
+    def next_num(model) do
+      %{ model |
+        num: model.num + 2,
+        divMax: Float.floor(Math.sqrt(model.num + 2)),
+        div: 3
+      }
+    end
+  end
 
   @doc """
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
-  def nth(count) do
+  def nth(1), do: 2
+  def nth(n) when n <= 0, do: raise "n can not be <= 0"
+  def nth(target_n) do
+    do_nth(%Model{target_n: target_n})
   end
 
+  defp do_nth(model) do
+    cond do
+      model.target_n == model.last_n -> model.last_prime
+      model.div > model.divMax ->
+        do_nth(%{ Model.next_num(model) | last_n: model.last_n + 1, last_prime: model.num})
+      rem(model.num, model.div) == 0 ->
+        do_nth(Model.next_num(model))
+      true ->
+        do_nth(%{ model | div: model.div + 2})
+    end
+  end
 end
