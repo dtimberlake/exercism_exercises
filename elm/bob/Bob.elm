f4c595a3a8fca850de940080d54be601099a67b1
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-14 16:49:19 -0500

    elixir/hamming initial fetch

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,1 +1,11 @@
 module Bob exposing (..)
+
+import Regex exposing(contains, regex)
+
+
+hey : String -> String
+hey str =
+  if (contains (regex "^[\s]*$") str) then
+    "Fine. Be that way!"
+  else
+    "abc"
