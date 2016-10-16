d911c97b0650ac13c56d66316096655bce08f7c6
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-16 00:22:58 -0500

    elixir isogram finished

--- elixir/isogram/isogram.exs
+++ elixir/isogram/isogram.exs
@@ -1,10 +1,13 @@
 defmodule Isogram do
-  @doc """
-  Determines if a word or sentence is an isogram
-  """
   @spec isogram?(String.t) :: boolean
   def isogram?(sentence) do
-
+    sentence = sentence |> String.replace(~r/[\s-]/, "")
+    sentence
+    |> String.to_charlist
+    |> MapSet.new
+    |> MapSet.size
+    |> is_equal(String.length(sentence))
   end
 
+  defp is_equal(a, b), do: a == b
 end
