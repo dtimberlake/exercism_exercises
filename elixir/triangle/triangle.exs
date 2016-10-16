0fab5583b524a16bc16bb43bf8af49662c528a46
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-15 23:52:00 -0500

    elixir triangle completed

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -1,11 +1,17 @@
 defmodule Triangle do
   @type kind :: :equilateral | :isosceles | :scalene
 
-  @doc """
-  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
-  """
   @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
-  def kind(a, b, c) do
+  def kind(a, b, c), do: [a, b, c] |> Enum.sort |> do_kind
 
+  defp do_kind([h | _]) when h <= 0, do: { :error, "all side lengths must be positive" }
+  defp do_kind([a, b, c]) when a + b <= c, do: { :error, "side lengths violate triangle inequality" }
+  defp do_kind(l) do
+    set = MapSet.new l
+    case MapSet.size(set) do
+      1 -> { :ok, :equilateral }
+      2 -> { :ok, :isosceles }
+      3 -> { :ok, :scalene }
+    end
   end
 end
