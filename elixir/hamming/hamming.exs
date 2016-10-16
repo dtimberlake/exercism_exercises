84add6ac0e52d6d0dfe43cbcde52e2f65348742f
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-15 23:29:01 -0500

    elixir hamming finished

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -7,7 +7,13 @@ defmodule Hamming do
   iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
   {:ok, 4}
   """
-  @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(strand1, strand2) do
+  @spec hamming_distance([char], [char]) :: {atom, non_neg_integer}
+  def hamming_distance(strand1, strand2), do: do_hamming_distance(strand1, strand2, 0)
+
+  defp do_hamming_distance(s1, s2, _) when length(s1) != length(s2), do: {:error, "Lists must be the same length"}
+  defp do_hamming_distance([], [], acc), do: {:ok, acc}
+  defp do_hamming_distance([h1 | t1], [h2 | t2], acc) do
+    acc = if h1 != h2, do: acc + 1, else: acc
+    do_hamming_distance(t1, t2, acc)
   end
 end
