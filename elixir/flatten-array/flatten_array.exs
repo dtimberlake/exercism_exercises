23acffe60208f432f70fc445e1509225e3edea35
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-16 01:46:35 -0500

    elixir flatten_array finished

--- elixir/flatten-array/flatten_array.exs
+++ elixir/flatten-array/flatten_array.exs
@@ -1,19 +1,10 @@
 defmodule FlattenArray do
-  @doc """
-    Accept a list and return the list flattened without nil values.
-
-    ## Examples
-
-      iex> FlattenArray.flatten([1, [2], 3, nil])
-      [1,2,3]
-
-      iex> FlattenArray.flatten([nil, nil])
-      []
-
-  """
-
   @spec flatten(list) :: list
-  def flatten(list) do
+  def flatten([]), do: []
+  def flatten(x), do: Enum.reverse(do_flatten(x, []))
 
-  end
+  defp do_flatten([], acc), do: acc
+  defp do_flatten([h | t], acc) when is_nil(h), do: do_flatten(t, acc)
+  defp do_flatten([h | t], acc) when is_list(h), do: do_flatten(t, do_flatten(h, acc))
+  defp do_flatten([h | t], acc), do: do_flatten(t, [h | acc])
 end
