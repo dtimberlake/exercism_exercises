d0bbf2c1340c1cc9e3916c4aef1ebc12cbf34c7e
Daniel Timberlake <daniel@danieltimberlake.com>
2016-10-18 16:49:09 -0500

    elixir binary finished

--- elixir/binary/binary.exs
+++ elixir/binary/binary.exs
@@ -1,10 +1,18 @@
 defmodule Binary do
-  @doc """
-  Convert a string containing a binary number to an integer.
+  import :math, only: [pow: 2]
 
-  On errors returns 0.
-  """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
+    string
+    |> String.split("", trim: true)
+    |> Enum.reverse
+    |> Enum.with_index
+    |> Enum.reduce_while(0, fn {s, index}, acc ->
+      case s do
+        "0" -> { :cont, acc }
+        "1" -> { :cont, acc + pow(2, index) }
+        _   -> { :halt, 0 }
+      end
+    end)
   end
 end
