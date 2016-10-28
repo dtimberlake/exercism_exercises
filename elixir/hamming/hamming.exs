7be33768cb7a6ecd7ecfd2150aded92de3e886ff
Daniel Timberlake <daniel@Daniels-MacBook-Pro.local>
2016-10-28 15:28:08 -0500

    refactored hamming.exs

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -1,19 +1,15 @@
 defmodule Hamming do
-  @doc """
-  Returns number of differences between two strands of DNA, known as the Hamming Distance.
-
-  ## Examples
-
-  iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
-  {:ok, 4}
-  """
   @spec hamming_distance([char], [char]) :: {atom, non_neg_integer}
-  def hamming_distance(strand1, strand2), do: do_hamming_distance(strand1, strand2, 0)
-
-  defp do_hamming_distance(s1, s2, _) when length(s1) != length(s2), do: {:error, "Lists must be the same length"}
-  defp do_hamming_distance([], [], acc), do: {:ok, acc}
-  defp do_hamming_distance([h1 | t1], [h2 | t2], acc) do
-    acc = if h1 != h2, do: acc + 1, else: acc
-    do_hamming_distance(t1, t2, acc)
+  def hamming_distance(stream1, stream2) do
+    if length(stream1) != length(stream2) do
+      { :error, "Lists must be the same length" }
+    else
+      { :ok, diff(stream1, stream2) }
+    end
+  end
+  
+  defp diff(stream1, stream2) do
+    Enum.zip(stream1, stream2)
+    |> Enum.count(fn {x, y} -> x != y end)
   end
 end
